e26ee5d6704bcbcdcbf7b4448e30ed69573f373e
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-06-18 21:36:36 +0630

    setup the file as duck folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-function App() {
-  return (
-    <h1>hello this is house app</h1>
-  );
-}
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import MiniNavbar from './components/Navbar/miniNavbar';
+import './index.css';
 
+const App = () => {
+  return ( 
+    <BrowserRouter>
+    <MiniNavbar />
+    </BrowserRouter>
+   );
+}
+ 
 export default App;
+
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect , useState } from 'react';
+import { Link, NavLink} from 'react-router-dom';
+
+const Navbar = () => {
+     return ( 
+
+      );
+}
+ 
+export default Navbar;
