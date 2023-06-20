bb4761decc2e9c0b8511a29facd5e3e63bbd9ba3
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-06-20 16:39:06 +0630

    Setup the navbar and header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
+import RentPage from './Pages/Rent/Rent';
 import './index.css';
 
 const App = () => {
   return ( 
     <BrowserRouter>
+    <div className='nav-section'>
     <Navbar />
+    <Header />
+    </div>
+    <RentPage />
     </BrowserRouter>
    );
 }
---
+++ src/Pages/Rent/Rent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RentPage = () => {
+     return ( 
+          <>
+          <section className='mx-auto w-full grid gap-3 grid-cols-1 md:grid-cols-2 mt-2'>
+          <div className="md:w-1/2">Hello</div>
+          <div className="md:w-1/2">World</div>
+          </section>
+
+          </>
+      );
+}
+ 
+export default RentPage;
